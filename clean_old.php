9b257ddcf25cd2f7781086f15d670dc31adcf209
Maxim Gabidullin <max@data-files.ru>
2023-05-15 22:19:33 +0300

    end-clean-old

--- clean_old.php
+++ clean_old.php
@@ -1,11 +1,26 @@
 <?php
 
+chdir('/var/www/shariki_backup');
 echo 'Clean old' . PHP_EOL;
 
+/**
+ * @param array $files
+ * @return void
+ */
+function delete_files(array $files): void
+{
+    if (count($files) > 3) {
+        $files = array_slice($files, 0, count($files) - 3);
+    } else {
+        return;
+    }
+    foreach ($files as $file) {
+        if (unlink($file)) {
+            echo 'Removed: ' . $file . PHP_EOL;
+        }
+    }
+}
 $files = glob('backups/shariki*');
-if (count($files) > 3) {
-   $files = array_slice($files, 0, count($files) - 3);
-}
-foreach ($files as $file) {
-    echo $file . PHP_EOL;
-}
+delete_files($files);
+$files = glob('backups/gelievyeshari24*');
+delete_files($files);
